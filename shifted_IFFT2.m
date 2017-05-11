9f22dafb80447951840f38d5ae051a33a07b2aba
bebbe <sebastian@mralbert.se>
2017-05-11 19:39:19 +0200

    changed the comment

--- shifted_IFFT2.m
+++ shifted_IFFT2.m
@@ -2,8 +2,8 @@
 This function takes the input and first swaps the first and third
     quadrants and the second and fourth quadrants since the fourier
     transform needs its input to be shifted. Then it transforms it with 2D
-    inverse fourier transform and shifts it again before returning it as a complex
-    N X N matrix.
+    inverse fourier transform and shifts it again before returning it as a 
+    complex N X N matrix.
 ---------------------------------------------------------------------------
 Input:
 
